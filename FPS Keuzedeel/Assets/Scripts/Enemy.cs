1f3754f2de4b2d7ffa533092b3c84d984179d704
Juul_Cox <juulcox00@gmail.com>
2019-02-19 11:06:00 +0100

    added score

--- FPS Keuzedeel/Assets/Scripts/Enemy.cs
+++ FPS Keuzedeel/Assets/Scripts/Enemy.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour, IEnemy
 {
     public NavMeshAgent agent;
     public GameObject player;
     // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         if(agent == null)
         agent = GetComponent<NavMeshAgent>();
+        if (player == null)
+        player = GameObject.Find("FPSController");
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class Enemy : MonoBehaviour, IEnemy
     }
     public void Action()
     {
+        PlayerController.score += 100;
         Destroy(this.gameObject);
     }
 }
--- FPS Keuzedeel/Assets/Scripts/PlayerController.cs
+++ FPS Keuzedeel/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float range = 2f;
     public float shootRange = 20f;
-
+    private Camera camera;
+    public static int score;
+    public Text Score;
     void Start()
     {
-        
+        score = 0;
+        camera = GetComponent<Camera>();
     }
 
     void Update()
@@ -23,11 +27,12 @@ public class PlayerController : MonoBehaviour
         {
             Shoot();
         }
+        Score.text = "Score = " + PlayerController.score.ToString();
     }
 
     void Interact()
     {
-        Ray r = new Ray(transform.position, this.gameObject.transform.forward);
+        Ray r = new Ray(transform.position, camera.transform.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(r, out hit, range))
@@ -41,7 +46,7 @@ public class PlayerController : MonoBehaviour
     }
     void Shoot()
     {
-        Ray r = new Ray(transform.position, this.gameObject.transform.forward);
+        Ray r = new Ray(transform.position, camera.transform.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(r, out hit, shootRange))
