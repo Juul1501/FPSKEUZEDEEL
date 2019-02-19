cd3dedbaf1e1bc63fb127fc7057e0f9a3d8305ce
Juul_Cox <juulcox00@gmail.com>
2019-02-19 11:10:50 +0100

    spawn enemies

---
+++ FPS Keuzedeel/Assets/SpawnEnemies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+    public float waitTime;
+    public int Amount;
+    public Text RoundCounter;
+
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > waitTime)
+        {
+            for (int i = 0; i < Amount; i++)
+            {
+                Instantiate(enemy, transform.position, transform.rotation);
+            }
+            timer = 0f;
+        }
+
+    }
+}
